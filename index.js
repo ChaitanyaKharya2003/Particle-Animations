8c518c303d57547d77d99260b37847ad7979682e
Chaitanya Kharya <kharyachaitanya@gmail.com>
2023-07-12 18:12:01 +0530

    defining individual pixels

--- index.js
+++ index.js
@@ -41,11 +41,26 @@ window.addEventListener("load", () => {
       this.centerY = this.height * 0.5;
       this.x = this.centerX - this.image.width * 0.5;
       this.y = this.centerY - this.image.height * 0.5;
+      this.gap = 5;
     }
     init(context) {
         context.drawImage(this.image, this.x, this.y);
-        const pixels = context.getImageData(0, 0, this.width, this.height);
-        
+        const pixels = context.getImageData(0, 0, this.width, this.height).data;
+      for (let y = 0; y < this.height; y += this.gap) {
+        for (let x = 0; x < this.width; x += this.gap){
+          const index = (y * this.width + x) * 4;
+          const red = pixels[index];
+          const green = pixels[index + 1];
+          const blue = pixels[index + 2];
+          const alpha = pixels[index + 3];
+          const color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+
+          if (alpha > 0) {
+            this.particlesArray.push(new Particle(this));
+          }
+        }
+      }
+          
     }
 
     draw(context) {
