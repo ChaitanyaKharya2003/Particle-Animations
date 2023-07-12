abbf7680bb5a016459d6d4fff69599a7e56d00a3
Chaitanya Kharya <kharyachaitanya@gmail.com>
2023-07-12 18:16:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ window.addEventListener("load", () => {
   const image5 = document.getElementById("img5");
 
   class Particle {
-    constructor(effect) {
+    constructor(effect, x, y, color) {
       this.effect = effect;
       this.x = Math.random() * this.effect.width;
       this.y = Math.random() * this.effect.height;
       this.size = 3;
+      this.originX = Math.floor(x);
+      this.originY = Math.floor(y);
+      this.color = color;
       this.vx = Math.random() * 2 - 1;
       this.vy = Math.random() * 2 - 1;
     }
@@ -44,23 +47,22 @@ window.addEventListener("load", () => {
       this.gap = 5;
     }
     init(context) {
-        context.drawImage(this.image, this.x, this.y);
-        const pixels = context.getImageData(0, 0, this.width, this.height).data;
+      context.drawImage(this.image, this.x, this.y);
+      const pixels = context.getImageData(0, 0, this.width, this.height).data;
       for (let y = 0; y < this.height; y += this.gap) {
-        for (let x = 0; x < this.width; x += this.gap){
+        for (let x = 0; x < this.width; x += this.gap) {
           const index = (y * this.width + x) * 4;
           const red = pixels[index];
           const green = pixels[index + 1];
           const blue = pixels[index + 2];
           const alpha = pixels[index + 3];
-          const color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+          const color = "rgb(" + red + "," + green + "," + blue + ")";
 
           if (alpha > 0) {
-            this.particlesArray.push(new Particle(this));
+            this.particlesArray.push(new Particle(this, x, y, color));
           }
         }
       }
-          
     }
 
     draw(context) {
