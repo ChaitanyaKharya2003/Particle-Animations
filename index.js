a250bbe330d238a4df8394a870df4faa13ea853d
Chaitanya Kharya <kharyachaitanya@gmail.com>
2023-07-13 11:22:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ window.addEventListener("load", () => {
       this.vx = 0;
       this.vy = 0;
       this.ease = 0.2;
+      this.dx = 0;
+      this.dy = 0;
+      this.distance = 0;
+      this.force = 0;
+      this.angle = 0;
+      this.friction = 0.95;
     }
 
     draw(context) {
@@ -34,9 +40,15 @@ window.addEventListener("load", () => {
       this.dy = this.effect.mouse.y - this.y;
       this.distance = (this.dx * this.dx + this.dy * this.dy);
       this.force = -this.effect.mouse.radius / this.distance;
-      this.x += (this.originX - this.x) * this.ease;
-      this.y += (this.originY - this.y) * this.ease;
       
+      if (this.distance < this.effect.mouse.radius) {
+        this.angle = Math.atan2(this.dy, this.dx);
+        this.vx = this.force * Math.cos(this.angle);
+        this.vy = this.force * Math.sin(this.angle);
+      }
+
+      this.x += (this.vx *= this.friction)+ (this.originX - this.x) * this.ease;
+      this.y +=  (this.vy *= this.friction) +(this.originY - this.y) * this.ease;
     }
     warp() {
       this.x = Math.random() * this.effect.width;
